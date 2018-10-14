55c778d1302f9664b0a4a73af448e44d238674c1
Rowan-James Tran <rowanjames.tran@gmail.com>
2018-10-13 17:23:07 -0700

    register

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { Input } from 'react-native-elements';
 import { createStackNavigator } from 'react-navigation';
 
 export default class RegisterScreen extends React.Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
+                <Input
+                    placeholder='Username'
+                    leftIcon={{ type: 'font-awesome', name: 'user' }}
+                />
+                <Input
+                    placeholder='Email'
+                    leftIcon={{ type: 'font-awesome', name: 'envelope-open' }}
+                    keyboardType='email-address'
+                />
+                <Input
+                    placeholder='Password'
+                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                    secureTextEntry={true}
+                />
+                <Input
+                    placeholder='Confirm password'
+                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                    secureTextEntry={true}
+                />
                 <Button onPress={() => {
                     Alert.alert("Registered.");
                 }}
@@ -14,3 +34,12 @@ export default class RegisterScreen extends React.Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
