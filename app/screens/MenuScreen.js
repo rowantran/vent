c508ae177b272e2a10ad5501b954f8567f201bf4
Hackathon Event <hackathon4@e1z1r7p6.42.us.org>
2018-10-14 03:30:28 -0700

    added blue squares to the menu

--- app/screens/MenuScreen.js
+++ app/screens/MenuScreen.js
@@ -43,7 +43,12 @@ export default class MenuScreen extends React.Component {
                     this.componentWillUnmount();
                 }}
                 title="Register" />
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} /> 
+                <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+
             </View>
+
         );
     }
 }
